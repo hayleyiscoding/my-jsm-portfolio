b7e7a158ced87d24ca1933c4397856e29bddafbe
Hayley <hayleyiscoding@gmail.com>
2023-09-17 23:33:13 +0800

    fix deployment error

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -26,16 +26,16 @@ const config: Config = {
       grayscale: {
         40: "40%",
       },
-      keyframes: {
-        "accordion-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
-        },
-      },
+      // keyframes: {
+      //   "accordion-down": {
+      //     from: { height: 0 },
+      //     to: { height: "var(--radix-accordion-content-height)" },
+      //   },
+      //   "accordion-up": {
+      //     from: { height: "var(--radix-accordion-content-height)" },
+      //     to: { height: 0 },
+      //   },
+      // },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
